b6e4db176b545b61e8a2cd2728bf67416f8db38d
Sivaskandharuban <65588953+Sivaskandharuban@users.noreply.github.com>
2020-11-10 21:42:04 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,18 @@
 var jsforce = require("jsforce");
-// Salesforce OAuth2 client informationvar conn = new jsforce.Connection({  oauth2: {    // you can change loginUrl to connect to sandbox or prerelease env.    loginUrl: "https://login.salesforce.com",    clientId: process.env.Consumer_Key,    clientSecret: process.env.Consumer_Secret,    redirectUri: process.env.Callback_URL,  },});
-//all the routes for our applicationmodule.exports = function (app, db, pgp) {  // =====================================  // HOME PAGE (with login links) ========  // ===================================== app.get("/api/getlead", function (req, res) {    //res.render("index.ejs"); // load the index.ejs file  const results = [];    var query = "SELECT * FROM lead ";    db.query(query, true)      .then(function (data) {        return res.json(data);      })      .catch(function (err) {        console.log("ERROR:", err); // print the error;        return res.status(400).json({ success: false, error: err });      })   .finally(function () {        pgp.end(); // for immediate app exit, closing the connection pool.      });  });
+ Salesforce OAuth2 client informationvar conn = new jsforce.Connection({  oauth2: {    
+	 // you can change loginUrl to connect to sandbox or prerelease env.    
+	 loginUrl: "https://login.salesforce.com",    
+	 clientId: process.env.Consumer_Key,    
+	 clientSecret: process.env.Consumer_Secret,    
+	 redirectUri: process.env.Callback_URL,  
+ },});
+//all the routes for our application
+module.exports = function (app, db, pgp) {  // =====================================  // HOME PAGE (with login links) ========  // ===================================== app.get("/api/getlead", function (req, res) {    //res.render("index.ejs"); // load the index.ejs file  const results = [];    var query = "SELECT * FROM lead ";    db.query(query, true)      .then(function (data) {        return res.json(data);      })      .catch(function (err) {        console.log("ERROR:", err); // print the error;        return res.status(400).json({ success: false, error: err });      })   .finally(function () {        pgp.end(); // for immediate app exit, closing the connection pool.      });  });
 app.get("/api/getPersons", function (req, res) {    
     //res.render("index.ejs"); 
     // load the index.ejs file  const results = [];    
-    var query = "SELECT * FROM persons ";    
+    var query = "SELECT * FROM persons ";
+	console.log("Query result", query);
 	db.query(query, true)      
     .then(function (data) {        
         return res.json(data);
@@ -20,6 +28,7 @@ app.get("/api/getPersons", function (req, res) {
 app.post("/api/createperson", function (req, res) {       
     const { address, city } = req.body;   
     var insertQuery ="INSERT INTO persons (personid, city) VALUES ('" +     address +     "','" +     city +     "')";
+	console.log("Query result", insertQuery);
     db.query(insertQuery, true)     
     .then(function (data) {       
         return res.json(data);     })     
