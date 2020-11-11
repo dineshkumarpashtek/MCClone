2034f18d3224ee2de5c8f72a45433660f4d56d4b
Sivaskandharuban <65588953+Sivaskandharuban@users.noreply.github.com>
2020-11-11 10:15:46 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -40,6 +40,6 @@ app.post("/api/createperson", function (req, res) {
 	    return res.status(400).json({ success: false, error: err });      })
 	       .finally(function () {        
                           pgp.end();      
-                        });
+                        }
 )});
 
