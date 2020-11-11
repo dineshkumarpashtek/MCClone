856c1e76395db67041cf5de7bd2c5361b4db705f
Sivaskandharuban <65588953+Sivaskandharuban@users.noreply.github.com>
2020-11-11 10:03:12 +0530

    Update routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,7 +7,14 @@ var jsforce = require("jsforce");
 	 redirectUri: process.env.Callback_URL,  
  },});
 //all the routes for our application
-module.exports = function (app, db, pgp) {  // =====================================  // HOME PAGE (with login links) ========  // ===================================== app.get("/api/getlead", function (req, res) {    //res.render("index.ejs"); // load the index.ejs file  const results = [];    var query = "SELECT * FROM lead ";    db.query(query, true)      .then(function (data) {        return res.json(data);      })      .catch(function (err) {        console.log("ERROR:", err); // print the error;        return res.status(400).json({ success: false, error: err });      })   .finally(function () {        pgp.end(); // for immediate app exit, closing the connection pool.      });  });
+module.exports = function (app, db, pgp) {  // =====================================  // HOME PAGE (with login links) ========  
+	// ===================================== app.get("/api/getlead", function (req, res) {    
+	//res.render("index.ejs"); 
+	// load the index.ejs file  const results = [];    
+	//var query = "SELECT * FROM lead ";    
+	//db.query(query, true)      
+	//.then(function (data) {        
+	//return res.json(data);      })      .catch(function (err) {        console.log("ERROR:", err); // print the error;        return res.status(400).json({ success: false, error: err });      })   .finally(function () {        pgp.end(); // for immediate app exit, closing the connection pool.      });  });
 app.get("/api/getPersons", function (req, res) {    
     //res.render("index.ejs"); 
     // load the index.ejs file  const results = [];    
@@ -28,7 +35,7 @@ app.get("/api/getPersons", function (req, res) {
 app.post("/api/createperson", function (req, res) {
 	console.log(req.body);
     const { address, city } = req.body;   
-    var insertQuery ="INSERT INTO persons (personid, city) VALUES ('" +     address +     "','" +     city +     "')";
+    var insertQuery ="INSERT INTO persons (address, city) VALUES ('" +     address +     "','" +     city +     "')";
 	console.log("Query result", insertQuery);
     db.query(insertQuery, true)     
     .then(function (data) {       
